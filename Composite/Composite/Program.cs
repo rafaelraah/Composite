8c0cfd9038c53d661b9b5fe2ad9d9e66e8a40df0
Rafael <rafaelsantos.rsdn@gmail.com>
2021-11-12 15:59:12 -0300

    First commit

---
+++ Composite/Composite/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesignPatterns;
+using System;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecucaoComposite.Executar();
+        }
+    }
+}
